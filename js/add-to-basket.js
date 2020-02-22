797841472a7377fcbc357796e00cc9849c3ea98a
Nikita <nikput05@gmail.com>
2020-02-22 19:10:07 +0300

    Улучшения для защиты

--- js/add-to-basket.js
+++ js/add-to-basket.js
@@ -3,7 +3,7 @@ var popup_open = document.querySelectorAll(".product-card__add-to-cart");
 var popup_close = document.querySelector(".added-to-basket-popup__close");
 
 for (var i = 0; i < popup_open.length; i++) {
-    popup_open[i].addEventListener('click', function (evt) {
+    popup_open[i].addEventListener("click", function (evt) {
         evt.preventDefault();
         popup.classList.add("modal__opened");
     });
